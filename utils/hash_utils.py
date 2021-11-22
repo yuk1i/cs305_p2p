2b0c09937061e25161fe887165facdf3ac1a88b5
yuk1i <chenjf2020@mail.sustech.edu.cn>
2021-11-22 14:04:06 +0800

    add hash_utils

--- utils/hash_utils.py
+++ utils/hash_utils.py
@@ -1,7 +1,14 @@
 def hash_bytes(data: bytes) -> str:
     pass
 
+
 def hash_file(file_path: str) -> str:
     pass
 
-def
+
+def hash_json_object(json_obj: dict) -> str:
+    pass
+
+
+def hash_json_array(json_array: list) -> str:
+    pass
