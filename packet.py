131e01cfff3f49a05cc016bcd3f5e7a809ea9a5e
yuki <chenjf2020@mail.sustech.edu.cn>
2021-11-25 23:27:09 +0800

    Refactor packet

--- packet.py
+++ packet.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from utils.bytes_utils import int_to_bytes, bytes_to_int
+from utils.bytes_utils import int_to_bytes, bytes_to_int, ByteWriter, ByteReader
 
 TYPE_NOTIFY = 0x05
 TYPE_REGISTER = 0x10
@@ -10,6 +10,22 @@ TYPE_REQUEST_TORRENT = 0x30
 TYPE_REQUEST_CHUNK_INFO = 0x31
 TYPE_REQUEST_CHUNK = 0x32
 TYPE_ACK = 0x20
+
+FLAG_REASSEMBLE = 0x40
+FLAG_SPEED_CONTROL = 0x20
+MASK_REVERSED = 0x3F
+
+
+class ReAssembleHeader:
+    def __init__(self, start: int = 0, length: int = 0, total_length: int = 0):
+        self.enabled = True
+        self.start = start
+        self.length = length
+        self.total_length = total_length
+
+
+NO_REASSEMBLE = ReAssembleHeader()
+NO_REASSEMBLE.enabled = False
 
 
 def deserialize_packet(data: bytes) -> BasePacket:
@@ -22,22 +38,91 @@ def deserialize_packet(data: bytes) -> BasePacket:
 
 
 class BasePacket:
+    """
+    Base Packet
+    For Incoming Packet:
+        set_data(raw_data) -> unpack(call unpack_header)
+    For Outgoing Packet
+        pack(call pack_header)
+    """
+
     def __init__(self, itype: int):
         self.type: int = itype
         self.reversed: int = 0
         self.identifier: int = 0
+        self.reassemble: ReAssembleHeader = NO_REASSEMBLE
+        self.__reader__ = None
+        self.__writer__ = None
+        self.__data__ = None
 
-    def unpack_header(self, data: bytes) -> BasePacket:
+    def get_header_length(self) -> int:
+        if self.reassemble.enabled:
+            return 16
+        else:
+            return 4
+
+    def set_data(self, data: bytes):
+        self.__data__ = data
+
+    def get_reader(self) -> ByteReader:
+        if self.__reader__:
+            return self.__reader__
+        self.__reader__ = ByteReader(self.__data__)
+        return self.__reader__
+
+    def get_writer(self) -> ByteWriter:
+        if self.__writer__:
+            return self.__writer__
+        self.__data__ = bytearray()
+        self.__writer__ = ByteWriter(self.__data__)
+        return self.__writer__
+
+    def unpack_header(self) -> ByteReader:
+        """
+        unpack header from bytes
+        :param reader: Byte Reader
+        """
         # self.type = data[0]
-        self.reversed = data[1]
-        self.identifier = bytes_to_int(data[2:2 + 2])
-        return self
+        reader = self.get_reader()
+        reader.skip(1)
+        rev = reader.read_byte()  # The second byte
+        self.reversed = rev & MASK_REVERSED
+        self.identifier = reader.read_short()
+        if rev & FLAG_REASSEMBLE == FLAG_REASSEMBLE:
+            # ReAssemble Enabled
+            self.reassemble = ReAssembleHeader(
+                start=reader.read_int(),
+                length=reader.read_int(),
+                total_length=reader.read_int())
+        return reader
+
+    def pack_header(self) -> ByteWriter:
+        """
+        pack the header, containing Type, Reversed, and Identifier
+        and Reassemble Header if needed
+        """
+        writer = self.get_writer()
+        writer.write_byte(self.type)
+        writer.write_byte(self.reversed & MASK_REVERSED)
+        writer.write_byte(self.identifier)
+        if self.reassemble.enabled:
+            writer.data[1] = writer.data[1] | FLAG_REASSEMBLE
+            writer.write_int(self.reassemble.start)
+            writer.write_int(self.reassemble.length)
+            writer.write_int(self.reassemble.total_length)
+        return writer
 
     def pack(self) -> bytes:
         pass
 
     def unpack(self, data: bytes) -> BasePacket:
         pass
+
+
+class ACKPacket(BasePacket):
+    def __init__(self, request: BasePacket):
+        super(ACKPacket, self).__init__(TYPE_ACK)
+        self.reversed = request.type & MASK_REVERSED
 
 
 class NotifyPacket(BasePacket):
@@ -48,20 +133,17 @@ class NotifyPacket(BasePacket):
         self.udp_port: int = 0
 
     def pack(self) -> bytes:
-        b = bytearray(17)
-        b[0] = self.type
-        b[1] = self.reversed
-        b[2:2 + 2] = int_to_bytes(self.identifier, 2)
-        b[4:4 + 8] = int_to_bytes(self.uuid, 8)
-        b[12:12 + 4] = int_to_bytes(self.ipv4_address, 4)
-        b[16:16 + 2] = int_to_bytes(self.udp_port, 2)
-        return b
+        writer = self.pack_header()
+        writer.write_long(self.uuid)
+        writer.write_int(self.ipv4_address)
+        writer.write_short(self.udp_port)
+        return writer.data
 
     def unpack(self, data: bytes) -> BasePacket:
-        self.unpack_header(data[0:0 + 4])
-        self.uuid = bytes_to_int(data[4:4 + 8])
-        self.ipv4_address = bytes_to_int(data[12:12 + 4])
-        self.udp_port = bytes_to_int(data[16:16 + 2])
+        r = self.unpack_header()
+        self.uuid = r.read_long()
+        self.ipv4_address = r.read_int()
+        self.udp_port = r.read_short()
         return self
 
 
--- utils/bytes_utils.py
+++ utils/bytes_utils.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import random
 
 
@@ -23,3 +24,65 @@ def random_int() -> bytes:
 
 def random_long() -> bytes:
     return random.randbytes(8)
+
+
+class ByteWriter:
+    def __init__(self, data: bytearray):
+        self.data = data
+
+    def len(self):
+        return len(self.data)
+
+    def write_byte(self, value: int) -> ByteWriter:
+        self.data.extend(int_to_bytes(value, 1))
+        return self
+
+    def write_short(self, value: int) -> ByteWriter:
+        self.data.extend(int_to_bytes(value, 2))
+        return self
+
+    def write_int(self, value: int) -> ByteWriter:
+        self.data.extend(int_to_bytes(value, 4))
+        return self
+
+    def write_long(self, value: int) -> ByteWriter:
+        self.data.extend(int_to_bytes(value, 8))
+        return self
+
+    def write_bytes(self, data: bytes) -> ByteWriter:
+        self.data.extend(data)
+        return self
+
+
+class ByteReader:
+    def __init__(self, data: bytes):
+        self.data = data
+        self.cursor = 0
+
+    def skip(self, _len: int):
+        self.cursor += _len
+
+    def read_byte(self) -> int:
+        ret = bytes_to_int(self.data[self.cursor:self.cursor + 1])
+        self.cursor += 1
+        return ret
+
+    def read_short(self) -> int:
+        ret = bytes_to_int(self.data[self.cursor:self.cursor + 2])
+        self.cursor += 2
+        return ret
+
+    def read_int(self) -> int:
+        ret = bytes_to_int(self.data[self.cursor:self.cursor + 4])
+        self.cursor += 4
+        return ret
+
+    def read_long(self) -> int:
+        ret = bytes_to_int(self.data[self.cursor:self.cursor + 8])
+        self.cursor += 8
+        return ret
+
+    def read_bytes(self, _len: int) -> bytes:
+        ret = self.data[self.cursor:self.cursor + _len]
+        self.cursor += _len
+        return ret
