acdec24365d6c52223a447632420c8f34cf6f3da
Trust_04zh <trust04zh@gmail.com>
2021-12-19 19:20:48 +0800

    move and fix traffic monitor hook to sockman

--- conn/socket_manager.py
+++ conn/socket_manager.py
@@ -32,12 +32,17 @@ class SocketManager:
             if not data and not self.proxy.active:
                 print("Socket Manager exited")
                 return
+            if src_addr not in self.mapper.keys():
+                peer = self.mapper[src_addr] = self.controller.accept_conn(src_addr)
+                if not peer:
+                    print("[Socket] Reject Connection From %s:%s" % src_addr)
+                    return
             self.mapper[src_addr].connectionStatus.feed_downlink(data)
             pkt = packet.deserializer.deserialize_packet(data)
             self.on_pkt_recv(src_addr, pkt)
 
-    def send_packet(self, pkt: BasePacket, dst_addr: IPPort, src_addr: IPPort):
-        peer: conn.Conn = self.mapper[src_addr]
+    def send_packet(self, pkt: BasePacket, dst_addr: IPPort):
+        peer: conn.Conn = self.mapper[dst_addr]
         if pkt.reassemble.enabled:
             pkts = conn.Assembler(pkt, self.mtu).boxing()
             for pdata in pkts:
@@ -60,13 +65,7 @@ class SocketManager:
             del self.mapper[addr]
 
     def on_pkt_recv(self, src_addr: IPPort, pkt: BasePacket):
-        iaddr = IPPort(src_addr[0], src_addr[1])
-        if iaddr not in self.mapper.keys():
-            peer = self.mapper[iaddr] = self.controller.accept_conn(src_addr)
-            if not peer:
-                print("[Socket] Reject Connection From %s:%s" % src_addr)
-                return
-        peer: conn.Conn = self.mapper[iaddr]
+        peer: conn.Conn = self.mapper[src_addr]
         fragment: bool = pkt.__data__[1] & FLAG_REASSEMBLE == FLAG_REASSEMBLE
         if fragment:
             identifier: int = bytes_utils.bytes_to_int(pkt.__data__[2:2 + 2])
