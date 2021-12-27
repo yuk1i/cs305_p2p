a1ae73e674decdf3766f26b06bd87bd0ca94197a
yuki <chenjf2020@mail.sustech.edu.cn>
2021-12-27 21:41:26 +0800

    Fix download alg

--- controller/download/__init__.py
+++ controller/download/__init__.py
@@ -1,8 +1,9 @@
 from .abstract_download_controller import AbstractDownloadController
 from .random_downloader import RandomDownloadController
-from .generate_downloader import generate_download_controller, DOWN_ALG_RANDOM
+from .generate_downloader import generate_download_controller, DOWN_ALG_RANDOM, DOWN_ALG_TITFORTAT
 
 __all__ = ['AbstractDownloadController',
            'RandomDownloadController',
            'generate_download_controller',
-           'DOWN_ALG_RANDOM']
+           'DOWN_ALG_RANDOM',
+           'DOWN_ALG_TITFORTAT']
--- controller/download/generate_downloader.py
+++ controller/download/generate_downloader.py
@@ -2,9 +2,11 @@ from . import RandomDownloadController
 from .titfortat_downloader import TitfortatDownloadController
 
 DOWN_ALG_RANDOM = 1
+DOWN_ALG_TITFORTAT = 2
 
 
 def generate_download_controller(itype) -> callable:
     if itype == DOWN_ALG_RANDOM:
+        return RandomDownloadController
+    elif type == DOWN_ALG_TITFORTAT:
         return TitfortatDownloadController
-
